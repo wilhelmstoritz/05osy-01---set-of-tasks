37b8b3f47cb0112d8e1f2fa12259627f02d19f4c
wilhelm storitz <wilhelm.x.storitz@gmail.com>
2025-10-04 09:01:34 +0200

    tutorial02 setup

---
+++ tutorial02/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int t_argc, char** t_argv) {
+    std::cout << "the future is bright!!\n";
+
+    return 0;
+}
